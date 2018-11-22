48b4a3320f557398638aed23815b92b8ea2ff945
tommy3531 <thomas.marler@hotmail.com>
2018-11-21 20:04:27 -0600

    Extend DemoApplication to passed HelloController to SpringApplication.run()

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import hello.HelloController;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -15,13 +16,13 @@ import java.util.Arrays;
 @SpringBootApplication
 public class DemoApplication {
 
-	@RequestMapping("/")
-	String home() {
-		return "Hello World!";
-	}
+//	@RequestMapping("/")
+//	String home() {
+//		return "Hello World!";
+//	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(HelloController.class, args);
 
 	}
 }
--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,11 +1,12 @@
 package hello;
 
+import com.example.demo.DemoApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class HelloController {
+public class HelloController extends DemoApplication {
 
     @RequestMapping("/")
     public String index() {
